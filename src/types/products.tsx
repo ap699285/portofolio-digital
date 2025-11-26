db7778708e0cd5d775c66c0eb3d203ee59d9baf4
ap699285 <79306994+ap699285@users.noreply.github.com>
2025-11-26 16:03:36 +0700

    update_schema

--- src/types/products.tsx
+++ src/types/products.tsx
@@ -2,11 +2,14 @@ import { StaticImageData } from "next/image";
 
 export type Product = {
   title: string;
+  restricted: boolean;
   description: string;
   thumbnail: StaticImageData;
   images: StaticImageData[] | string[];
   href: string;
   slug?: string;
   stack?: string[];
+  responsibility?: string[];
+  features?: string[];
   content?: React.ReactNode | string;
 };
